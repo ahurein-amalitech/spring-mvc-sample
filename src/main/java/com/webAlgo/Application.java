0d9317313679d1c739161c76e16009d7f2f5a370
ahurein-amalitech <ebenezer.ahurein@amalitech.com>
2024-07-03 16:24:00 +0000

    feat: basic rest/mvc

---
+++ src/main/java/com/webAlgo/Application.java
@@ -0,0 +1,12 @@
+package com.webAlgo;
+
+import com.webAlgo.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+}
---
+++ src/main/java/com/webAlgo/config/ThymeleafConfig.java
@@ -0,0 +1,8 @@
+package com.webAlgo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+
+public class ThymeleafConfig {
+
+}
